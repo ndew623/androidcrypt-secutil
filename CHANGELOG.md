f085fc44e065765495face760b5eb7115f777df0
Paul E. Jones <paulej@packetizer.com>
2024-12-01 21:56:42 -0500

    Improvements to SecureAllocator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,13 @@
 # Change Log
 
-1.0.1
-    - Test for explicit_bzero() on Linux, as that was only introduced in 2017
+v1.0.2
+
+- Improved the SecureAllocator object.
+
+v1.0.1
+
+- Test for explicit_bzero() on Linux, as that was only introduced in 2017
 
 v1.0.0
-    - Initial Release
+
+- Initial Release
--- test/secure_types/test_secure_types.cpp
+++ test/secure_types/test_secure_types.cpp
@@ -15,6 +15,7 @@
  *      None.
  */
 
+#include <string>
 #include <terra/secutil/secure_string.h>
 #include <terra/secutil/secure_vector.h>
 #include <terra/secutil/secure_deque.h>
@@ -22,6 +23,21 @@
 #include <terra/stf/stf.h>
 
 using namespace Terra;
+
+// Simple object for testing
+class SomeObject
+{
+    public:
+        SomeObject() {};
+        SomeObject(const char *text) : data{text} {}
+        SomeObject(std::string &text) : data{text} {}
+        ~SomeObject() = default;
+        void AssignData(std::string &text) { data = text; }
+        std::string GetData() { return data; }
+
+    protected:
+        std::string data;
+};
 
 // Just ensure we can build and strings compare properly
 STF_TEST(SecureTypes, SecureString)
@@ -32,11 +48,40 @@ STF_TEST(SecureTypes, SecureString)
     STF_ASSERT_EQ(s, q);
 }
 
-STF_TEST(SecureTypes, SecureVector)
+STF_TEST(SecureTypes, SecureVector1)
 {
     SecUtil::SecureVector<int> vec = {1, 2, 3};
 
     STF_ASSERT_EQ(3, vec.size());
+}
+
+STF_TEST(SecureTypes, SecureVector2)
+{
+    SecUtil::SecureVector<SomeObject> vec;
+
+    vec.emplace_back("String 1");
+    vec.emplace_back("String 2");
+    vec.emplace_back("String 3");
+    vec.emplace_back("String 4");
+
+    STF_ASSERT_EQ(std::string("String 3"), vec[2].GetData());
+
+    STF_ASSERT_EQ(4, vec.size());
+}
+
+STF_TEST(SecureTypes, SecureVector3)
+{
+    SecUtil::SecureVector<int> vec;
+
+    vec.emplace_back(1);
+    vec.emplace_back(2);
+    vec.emplace_back(3);
+    vec.emplace_back(4);
+    vec.emplace_back(5);
+
+    STF_ASSERT_EQ(3, vec[2]);
+
+    STF_ASSERT_EQ(5, vec.size());
 }
 
 STF_TEST(SecureTypes, SecureDeque)
